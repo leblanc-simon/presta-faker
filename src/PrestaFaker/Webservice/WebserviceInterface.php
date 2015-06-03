349e316938f55c48c73f074f98967bf081a6ce90
Simon Leblanc <contact@leblanc-simon.eu>
2015-06-03 05:28:32 +0200

    Fix Sensio Insight Report

--- src/PrestaFaker/Webservice/WebserviceInterface.php
+++ src/PrestaFaker/Webservice/WebserviceInterface.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the PrestaFaker package.
+ *
+ * (c) Simon Leblanc <contact@leblanc-simon.eu>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace PrestaFaker\Webservice;
 
 use Symfony\Component\EventDispatcher\EventDispatcher;
